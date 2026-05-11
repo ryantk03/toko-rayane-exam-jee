18ff836b41e7616b5e0916a6adb17c071b5c3dff
ryntk <rayanetoko@proton.me>
2026-05-11 14:36:10 +0100

    init backend project

---
+++ exam-jee-backend/src/main/java/tech/tokorayane/examjeebackend/ExamJeeBackendApplication.java
@@ -0,0 +1,13 @@
+package tech.tokorayane.examjeebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamJeeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamJeeBackendApplication.class, args);
+    }
+
+}
---
+++ exam-jee-backend/src/test/java/tech/tokorayane/examjeebackend/ExamJeeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.tokorayane.examjeebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamJeeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
